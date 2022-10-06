35de0a85378c9efc77495bd8f54d88411a52c1ea
dediananto <dedi.ananto@sirclo.com>
2022-10-06 14:03:46 +0700

    style(custom font): montserrat

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{asset('assets/images/fairytail.png')}}">
     <title>{{$title}}</title>
     @vite('resources/css/app.css')
 </head>
