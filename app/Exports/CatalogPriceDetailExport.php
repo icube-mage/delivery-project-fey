e6fac3838e7020450e3641daae44e57b4e451e1f
dediananto <dedi.ananto@sirclo.com>
2022-10-17 16:11:10 +0700

    perf(add chunk in import and export): improve for large data

--- app/Exports/CatalogPriceDetailExport.php
+++ app/Exports/CatalogPriceDetailExport.php
@@ -26,9 +26,24 @@ class CatalogPriceDetailExport implements FromCollection,WithHeadings
     } 
     public function collection()
     {
-        return CatalogPrice::select('marketplace', 'brand', 'sku', 'catalog_prices.product_name as product', 'retail_price','discount_price','users.name', 'catalog_prices.created_at as date')
+        $catalogPricesAll = collect([]);
+        CatalogPrice::select('marketplace', 'brand', 'sku', 'catalog_prices.product_name as product', 'retail_price','discount_price','users.name', 'catalog_prices.created_at as date')
         ->leftJoin('users', 'catalog_prices.user_id', '=', 'users.id')
         ->where('upload_hash', $this->hash)
-        ->get();
+        ->chunk(1000, function ($catalogPrices) use($catalogPricesAll){
+            foreach ($catalogPrices as $catalogPrice) {
+                $catalogPricesAll->push([
+                    "marketplace"=>$catalogPrice->marketplace,
+                    "brand"=>$catalogPrice->brand,
+                    "sku"=>$catalogPrice->sku,
+                    "product"=>$catalogPrice->product,
+                    "retail_price"=>$catalogPrice->retail_price,
+                    "discount_price"=>$catalogPrice->discount_price,
+                    "name"=>$catalogPrice->name,
+                    "date"=>$catalogPrice->date,
+                ]);
+            }
+        });
+        return $catalogPricesAll;
     }
 }
