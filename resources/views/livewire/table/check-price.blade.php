13706167ddfcfb681a01f3b438666a780acf9ed6
salmanseif <salman.seif@mail.sirclo.com>
2022-10-18 17:00:09 +0700

    fix(Fix bug select all): add handling if not all data selected & all select

--- resources/views/livewire/table/check-price.blade.php
+++ resources/views/livewire/table/check-price.blade.php
@@ -67,7 +67,7 @@
                 </x-th>
                 <x-th>
                     <div class="flex items-center self-center">
-                        <input type="checkbox" wire:change="selectAllWhitelist($event.target.checked)" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2">
+                        <input type="checkbox" wire:model="checkAll" wire:change="selectAllWhitelist($event.target.checked)" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2">
                         <label for="default-checkbox" class="ml-2">Select All</label>
                     </div>
                 </x-th>
