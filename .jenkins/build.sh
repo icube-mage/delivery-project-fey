ef66d15410c873b73a942e3d95108355092d6691
riansakti <rian.sakti@sirclo.com>
2022-11-11 09:05:44 +0700

    fixing bug release to prod in the next day

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -4,14 +4,12 @@ if [ -z "$1" ] ; then
         exit 1
 fi
 bid=$(echo $BUILD_ID)
-ts=$(date +"%Y%m%d")
 githead=$(git rev-parse HEAD)
 set -ex
-echo "Building Backend Docker image with tag " $$githead-$$ts$$bid
-echo $githead-$ts$bid
+echo "Building Backend Docker image with tag " $$githead-$$bid
 
 docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io
 
 # Build apps image
-docker build -t asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$ts$bid -f Dockerfile .
-docker push asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$ts$bid
+docker build -t asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$bid -f Dockerfile .
+docker push asia.gcr.io/sirclo-iii-nonprod/$1-dev:$githead-$bid
